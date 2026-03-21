095eec01814ce720424cf094cb97aa08ba4b9c19
Simon Danielsson <116196524+simon-danielsson@users.noreply.github.com>
2026-03-21 13:42:13 +0100

    refactor (main.rs:19): refactor cwd logic

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cargo build --release
+
+/Users/simondanielsson/dev/rust/raket/target/release/raket
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,45 @@
 // *brakoll - d: init readme and license, p: 100, t: docs, s: closed
+// *brakoll - d: color enums to avoid hardcoding ansi color codes, p: 50, t: feature, s: open
+
+use std::env;
+use std::process::Command;
 
 fn main() {
-    println!("Hello, world!");
+    let branch = get_git_branch();
+    let cwd = get_cwd();
+
+    let prompt = match branch {
+        Some(b) => format!("\x2b[32m{}\x1b[0m (\x1b[33m{}\x1b[0m) $ ", cwd, b),
+        None => format!("\x2b[32m{}\x1b[0m $ ", cwd),
+    };
+
+    print!("{}", prompt);
+}
+
+// *brakoll - d: refactor cwd logic, p: 100, t: refactor, s: closed
+fn get_cwd() -> String {
+    let cd = env::current_dir()
+        .ok()
+        .map(|s| s.to_string_lossy().into_owned())
+        .unwrap_or("?".into());
+
+    let home = env::home_dir()
+        .map(|s| s.to_string_lossy().into_owned())
+        .unwrap_or("?".into());
+
+    cd.replace(&home, "~")
+}
+
+fn get_git_branch() -> Option<String> {
+    let output = Command::new("git")
+        .args(["rev-parse", "--abbrev-ref", "HEAD"])
+        .output()
+        .ok()?;
+
+    if !output.status.success() {
+        return None;
+    }
+
+    let branch = String::from_utf8_lossy(&output.stdout).trim().to_string();
+    Some(branch)
 }
